6fd2c2c5f489e722f58530b7c4861a9746958314
Priya <priya@gmail.com>
2024-09-17 15:09:44 +0530

    testcase

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package javabean;
+
+public class MainClass {
+	public static void main(String args[]) {
+		car c = new car();
+		c.setBrand("Mercedes Benz");
+		System.out.println("Brand: "+c.getBrand());
+	}
+
+}
